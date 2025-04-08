3991fbc2b9ca3181f282393c65291cd7efb3c8ab
TarunVermaaa <tarun.it1604@gmail.com>
2025-04-08 14:28:12 +0530

    LogutFunctionality

--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -10,6 +10,10 @@ import {
 } from "lucide-react";
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
 
 const sideBarItems = [
   { Icon: <Home />, text: "Home" },
@@ -31,7 +35,32 @@ const sideBarItems = [
 ];
 
 const LeftSidebar = () => {
-  
+  const navigate = useNavigate()
+
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.get(" http://localhost:8000/api/v1/user/logout", {withCredentials: true});
+      if( res.data.success) {
+        navigate("/login")
+        toast.success(res.data.message)
+      }
+      
+    } catch (error) {
+      console.error("Logout failed", error);
+      toast.error(error.response.data.message);
+    }
+  }
+
+
+  const sidebarHandler = (textType) => {
+    if (textType === "Logout") {
+      logoutHandler()
+    }
+    if (textType === "Home") {
+      navigate("/")
+    }
+  }
+
   return (
     <div className="fixed top-8 z-10 left-0 px-4 border-r border-gray-300 w-[16%] h-screen">
       <div className="flex flex-col">
@@ -40,6 +69,7 @@ const LeftSidebar = () => {
           {sideBarItems.map((item, index) => {
             return (
               <div
+               onClick={ () => sidebarHandler(item.text)}
                 key={index}
                 className="flex items-center gap-3 relative p-2 hover:bg-gray-100 cursor-pointer rounded-lg my-3"
               >
